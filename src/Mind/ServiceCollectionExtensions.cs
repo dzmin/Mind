678d361f65370268bca4835c717526f3f5bf4162
dozm <dozm@outlook.com>
2017-08-23 20:19:24 +0800

    创建解决方案

---
+++ src/Mind/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Mind
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMindCore(this IServiceCollection collection)
+        {
+
+            return collection;
+        }
+    }
+}
